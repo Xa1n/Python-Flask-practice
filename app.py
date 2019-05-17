172580e8f78f1d4e75de928185849c7e3aecb7a7
Xa1n <xhs893@gmail.com>
2019-05-17 14:22:44 +0100

    Adds default route for page, using flask methods to get an optional name as an argument, and render it in the index file. Placed index.html file in 'templates' directory so flask recognises it

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)       # command needed at the top of every Flask application - means turn this file into a web application
 
 @app.route("/")         #listen now for a GET request to the "/" route
-def index():  
-    return "Hello Flask"          # index is arbitrary
+def index():                    #index is arbitrary
+    name = request.args.get("name")
+    return render_template("index.html", name=name)                #render_template takes multiple args   
